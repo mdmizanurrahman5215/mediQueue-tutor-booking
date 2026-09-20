22b5764b093478aab137a0a7a7018a1803339a01
Mizanur Rahman <mizanurrahman@ibos.io>
2026-09-20 15:12:48 +0600

    add suspense at addTutorPage

--- app/(admin)/add-tutor/page.jsx
+++ app/(admin)/add-tutor/page.jsx
@@ -1,10 +1,19 @@
 import AddTutorForm from '@/app/components/pages/AddTutorPage'
-import React from 'react'
+import { Loader2 } from 'lucide-react'
+import React, { Suspense } from 'react'
 
 const AddTutorPage = () => {
   return (
     <div>
+      <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-950">
+          <Loader2 className="w-8 h-8 animate-spin text-blue-600" />
+        </div>
+      }
+    >
       <AddTutorForm/>
+    </Suspense>
     </div>
   )
 }
